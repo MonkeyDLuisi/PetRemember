c67dfcbd3e5604d00fe343dfecb1cdba8f9b8494
Luis Aitor Bahillo Alonso <LuisAitor.Bahillo@e-msoffice.uab.cat>
2021-02-25 23:27:41 +0100

    User en BBDD. Hay que ejecutar en PM: Add-Migration InitialCreate Update-Database

---
+++ PetRemember/Models/Data/PetRememberContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using PetRemember.Models;
+
+namespace PetRemember.Data
+{
+    public class PetRememberContext : DbContext
+    {
+        public PetRememberContext(DbContextOptions<PetRememberContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<User> User { get; set; }
+    }
+}
