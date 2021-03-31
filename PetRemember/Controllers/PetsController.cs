42e6d5b5abae4feebce046770c6bb2a9b76ecaa3
Luis Aitor Bahillo Alonso <LuisAitor.Bahillo@e-campus.uab.cat>
2021-03-31 18:32:49 +0200

    register pet

---
+++ PetRemember/Controllers/PetsController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PetRemember.Application;
+using PetRemember.Domain.Pets;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PetRemember.Controllers
+{
+    public class PetsController : Controller
+    {
+        private readonly IPetService _petService;
+        public PetsController(IPetService petService)
+        {
+            _petService = petService;
+        }
+        public IActionResult Register()
+        {
+            Guid id;
+            if (Guid.TryParse(HttpContext.Session.GetString("userId"), out id))
+            {
+                return View();
+            }
+            else
+            {
+                return GoHome();
+            }
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Register(Pet pet)
+        {
+            if (ModelState.IsValid)
+            {
+                pet.UserId = Guid.Parse(HttpContext.Session.GetString("userId"));
+                _petService.Add(pet);
+                return RedirectToAction(nameof(UsersController.Details), "Users", new { Id = pet.UserId });
+            }
+            return GoHome();
+        }
+        private IActionResult GoHome()
+        {
+            return RedirectToAction(nameof(HomeController.Index), "Home");
+        }
+    }
+}
