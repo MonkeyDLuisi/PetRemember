5b9927132b4edcb7b9744b1556ce57983018dcc4
Luis Aitor Bahillo Alonso <LuisAitor.Bahillo@e-campus.uab.cat>
2021-05-11 20:13:23 +0200

    user infrastructure

--- PetRemember.Infrastructure/SQL/UserRepository.cs
+++ PetRemember.Infrastructure/SQL/UserRepository.cs
@@ -2,21 +2,29 @@
 using System;
 using System.Collections.Generic;
 using System.Data;
+using System.Data.SqlClient;
 using System.Text;
+using Dapper;
 
 namespace PetRemember.Infrastructure.SQL
 {
     public class UserRepository : IUserRepository
     {
+        private readonly SqlConnection _sqlConnection;
+        public UserRepository()
+        {
+            _sqlConnection = new SqlConnection("Server=(localdb)\\mssqllocaldb;Database=PetRememberContext-1;Trusted_Connection=True;MultipleActiveResultSets=true");
+        }
         public Guid Add(User user)
         {
-            return Guid.NewGuid();
+            var userId = _sqlConnection.ExecuteScalar("insert into Users (Mail, Password, Salt, FullName) output inserted.Id values (@Mail, @Password, @Salt, @FullName)", new { Mail = user.Mail, Password = user.Password, Salt = user.Salt, FullName = user.FullName });
+            return (Guid)userId;
         }
 
         public User Get(string mail)
         {
-            var salt = Hmac.GenerateSalt();
-            return new User() { FullName = "Mockeado", Id = Guid.NewGuid(), Salt = salt, Password = Hmac.ComputeHMAC_SHA256(Encoding.UTF8.GetBytes("test"), salt) };            
+            var user = _sqlConnection.QueryFirst<User>("select * from Users where Mail = @mail", new { mail = mail });
+            return user;            
         }
 
         public User Get(Guid id)
